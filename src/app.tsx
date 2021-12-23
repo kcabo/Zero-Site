8838637e8a29a33999539ddf7320a11c9a0f8859
kcabo <37576764+kcabo@users.noreply.github.com>
2021-12-23 07:18:03 +0000

    📦️ installed tailwindcss

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,20 @@
-import { Logo } from './logo'
+import { Logo } from './logo';
 
 export function App() {
   return (
     <>
       <Logo />
-      <p>Hello Vite + Preact!</p>
+      <p className='text-lg font-bold text-red-700'>Hello Vite + Preact!</p>
       <p>
         <a
-          class="link"
-          href="https://preactjs.com/"
-          target="_blank"
-          rel="noopener noreferrer"
+          class='link'
+          href='https://preactjs.com/'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn Preact
         </a>
       </p>
     </>
-  )
+  );
 }
